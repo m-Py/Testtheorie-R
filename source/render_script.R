d0d91c8b6a1890d2d1a43ec21bfc0f34d4d4cca2
Martin Papenberg <martin.papenberg@hhu.de>
2019-01-04 12:53:31 +0100

    Kommentare zum rendern des Skripts hinzugefügt

--- source/render_script.R
+++ source/render_script.R
@@ -1,3 +1,12 @@
 
+## Um das Skript zu generieren, muss wie folgt ein Datensatz heruntergeladen werden 
+## und in diesem Verzeichnis abgelegt werden:
+
+## Unter https://openpsychometrics.org/_rawdata/ 
+## -> Zeile "Answers to the Narcissistic Personality Inventory, constructed with the version from Raskin and Terry (1988)."
+## -> Spalte "Download"
+## -> zip-Datei entpacken und die Datei "data.csv" hier im Verzeichnis ablegen
+
+## Dann diesen Code ausführen:
 library("rmarkdown")
 render("01_Einstieg.Rmd")
