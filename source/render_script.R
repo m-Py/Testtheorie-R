37855cf804b3dccd8af71dc941b18fff49f6247e
Martin Papenberg <martin.papenberg@hhu.de>
2020-03-23 16:54:03 +0100

    direkter Download der Daten beim Rendern

--- source/render_script.R
+++ source/render_script.R
@@ -1,14 +1,4 @@
 
-## Um das Skript zu generieren, muss wie folgt ein Datensatz heruntergeladen werden
-## und in diesem Verzeichnis abgelegt werden:
-
-## Unter https://openpsychometrics.org/_rawdata/
-## -> Zeile "Answers to the Narcissistic Personality Inventory, constructed with the version from Raskin and Terry (1988)."
-## -> Spalte "Download"
-## -> zip-Datei entpacken und die Datei "data.csv" hier im Verzeichnis ablegen
-
-## Dann diesen Code ausführen:
-library("rmarkdown")
 library("bookdown")
 render("00_Setup.Rmd", "bookdown::pdf_book")
 render("00_Setup.Rmd", "bookdown::gitbook",
