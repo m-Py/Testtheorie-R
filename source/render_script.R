656a843a5969e8a3085fedff17ea72e0e4f5ac0e
Martin Papenberg <martin.papenberg@hhu.de>
2020-03-24 12:37:50 +0100

    man musste doch rmarkdown laden

--- source/render_script.R
+++ source/render_script.R
@@ -1,5 +1,6 @@
 
 library("bookdown")
+library("rmarkdown")
 render("00_Setup.Rmd", "bookdown::pdf_book")
 render("00_Setup.Rmd", "bookdown::gitbook",
        output_options = list(css = "style.css"))
